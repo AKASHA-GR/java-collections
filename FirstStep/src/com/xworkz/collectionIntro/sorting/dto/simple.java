37097e01d2d39365bace74893ee7f2dc504865cc
Akasha777 <akashmlk2021@gmail.com>
2026-07-08 23:56:08 +0530

    understanding the sort and compareTo method

---
+++ FirstStep/src/com/xworkz/collectionIntro/sorting/dto/simple.java
@@ -0,0 +1,14 @@
+package com.xworkz.collectionIntro.sorting.dto;
+
+import java.util.*;
+
+public class simple {
+    public static void main(String[] args) {
+        List<Integer> numbers = Arrays.asList(50, 10, 40, 20, 30);
+
+        Collections.sort(numbers);
+
+        System.out.println(numbers);
+    }
+}
+
