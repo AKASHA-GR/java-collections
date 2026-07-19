03754a939129f7a231016a8c09f49e891d51eba4
Akasha777 <akashmlk2021@gmail.com>
2026-07-19 23:52:16 +0530

    Introduction to Map

---
+++ Final_Step/src/com/xworkz/map/Simple.java
@@ -0,0 +1,36 @@
+package com.xworkz.map;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class Simple {
+    public static void main(String[] args) {
+
+        Map<Integer,String> map = new HashMap<>();
+
+        map.put(100,"Akasha");
+        map.put(150,"Hemanth");
+        map.put(200,"Krishna");
+        map.put(250,"Prateek");
+        map.put(300,"Sagar");
+
+        System.out.println(map);
+        System.out.println();
+
+
+        Map<Integer,String> map1 = new TreeMap<>();
+        map1.put(100,"Akasha");
+        map1.put(150,"Hemanth");
+        map1.put(200,"Krishna");
+        map1.put(250,"Prateek");
+        map1.put(300,"Sagar");
+
+        System.out.println(map1);
+        System.out.println();
+
+
+
+
+    }
+}
